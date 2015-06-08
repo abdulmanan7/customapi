0493290fa75becc008d1001f7a29d40a61ed35de
Abdul Manan <abdulmanan7@hotmail.com>
2015-06-08 18:27:16 +0500

    updated

--- application/controllers/customer.php
+++ application/controllers/customer.php
@@ -7,30 +7,33 @@ class Customer extends CI_Controller {
     protected $secret_key;
     protected $app_key;
     protected $bank_app_key="eY/etUTuQV9SFC/nTXXT3S6jE8kI6GxDiC9ugjEIjJz9DkhRtPn4cQdJZiElb1Bqv/b32Tj4F8YGpXZwl3q6Jw==";
-	/**
-	 
-	 */
 	function __construct() {
 		parent::__construct();
         $this->load->model('customapi_model');
     }
     public function index()
     {
-        if ($this->input->post()) {
-            if ($this->customapi_lib->validate_user($this->userId,$this->secret_key)) {
-                echo "You are not authorize user !";
-            }
-            else{
-                $this->prepData();
-            }
-        }
-        if (isset($this->userId)) {
-        $data="Please Provide Payment Details to Process!";
-        $this->load->view('customer', $data);
-        }
-        else{
-            $this->load->view('login');
-        }
+        $this->prepData();
+        // $sdata = "eY/etUTuQV9SFC/nTXXT3S6jE8kI6GxDiC9ugjEIjJz9DkhRtPn4cQdJZiElb1Bqv/b32Tj4F8YGpXZwl3q6Jw==1433515371http://www.bazmoapps.com/nibssagg/api/Payment?SchoolId=DEMO&PaymentReference=AA129GET";
+        // $encrypted_sdata=$this->customapi_lib->createHmacsha($sdata);
+        // print_r($encrypted_sdata);
+        // // $this->encrypt();
+        // die;
+        // if ($this->input->post()) {
+        //     if ($this->customapi_lib->validate_user($this->userId,$this->secret_key)) {
+        //         echo "You are not authorize user !";
+        //     }
+        //     else{
+        //         $this->prepData();
+        //     }
+        // }
+        // if (isset($this->userId)) {
+        // $data="Please Provide Payment Details to Process!";
+        // $this->load->view('customer', $data);
+        // }
+        // else{
+        //     $this->load->view('login');
+        // }
 	}
     private function get_user($id)
     {
@@ -47,10 +50,11 @@ class Customer extends CI_Controller {
         //Uri uri = new Uri(String.Format("http://www.bazmoapps.com/nibssagg/api/Payment?SchoolId={0}&PaymentReference={1}", schoolId, paymentReference));
         //$url=urlencode("http://localhost/projects/customapi/customer/customer?SchoolId=DEMO&PaymentReference=AA127");
 
-        $url="http://www.bazmoapps.com/nibssagg/api/Payment?SchoolId=DEMO&PaymentReference=AA129GET";
+        $url="http://www.bazmoapps.com/nibssagg/api/Payment?SchoolId=DEMO&PaymentReference=AA127GET";
+        // $url="http://localhost/projects/customapi/customer?SchoolId=DEMO&PaymentReference=AA127GET";
 
         $sdata= $this->bank_app_key.$utc.$url;
-        // $sdata= "eY/etUTuQV9SFC/nTXXT3S6jE8kI6GxDiC9ugjEIjJz9DkhRtPn4cQdJZiElb1Bqv/b32Tj4F8YGpXZwl3q6Jw==1433510862http://www.bazmoapps.com/nibssagg/api/Payment?SchoolId=DEMO&PaymentReference=AA129GET";
+        // $sdata= "eY/etUTuQV9SFC/nTXXT3S6jE8kI6GxDiC9ugjEIjJz9DkhRtPn4cQdJZiElb1Bqv/b32Tj4F8YGpXZwl3q6Jw==1433515371http://www.bazmoapps.com/nibssagg/api/Payment?SchoolId=DEMO&PaymentReference=AA129GET";
         $encrypted_sdata=$this->customapi_lib->createHmacsha($sdata);
         // echo $encrypted_sdata."<br/>" ;die;
        $result = array(
@@ -61,8 +65,8 @@ class Customer extends CI_Controller {
                         'app_key'=>$this->bank_app_key 
                        );
 
-       // $this->request($result);
-        echo "<pre>";print_r($result);echo "</pre>";
+       $this->request($result);
+        // echo "<pre>";print_r($result);echo "</pre>";
     }
     public function init_user()
     {
@@ -72,20 +76,24 @@ class Customer extends CI_Controller {
         }
     }
     function request($data) {
+    $header_data = array(
+            "Content-Type: application/x-www-form-urlencoded",
+            "X-PSK:".$data['app_key'],
+            "X-Stamp:".$data['Stamp'],
+            "X-Signature:".$data['hash']
+        );
+    // $header_data = array(
+    //         "Content-Type: application/x-www-form-urlencoded",
+    //         "X-PSK:eY/etUTuQV9SFC/nTXXT3S6jE8kI6GxDiC9ugjEIjJz9DkhRtPn4cQdJZiElb1Bqv/b32Tj4F8YGpXZwl3q6Jw==",
+    //         "X-Stamp:1433593864",
+    //         "X-Signature:7GGJBTNUZHeKjEcwc75qabyw+kG4+1zMK8cOf36PUiU="
+    //     );
     $ch = curl_init();
     $curlOpts = array(
-        // CURLOPT_URL => 'www.bazmoapps.com/nibssagg/api/Payment?SchoolId=DEMO&PaymentReference=AA127',
-        CURLOPT_URL => 'http://localhost/projects/customapi/customer?SchoolId=DEMO&PaymentReference=AA127',
+        CURLOPT_URL => 'www.bazmoapps.com/nibssagg/api/Payment?SchoolId=DEMO&PaymentReference=AA127',
+        // CURLOPT_URL => 'http://localhost/projects/customapi/customer?SchoolId=DEMO&PaymentReference=AA127',
         CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_HTTPHEADER => array(
-            "Content-Type: application/x-www-form-urlencoded",
-            // "X-PSK:".$data['app_key'],
-            "X-PSK:eY/etUTuQV9SFC/nTXXT3S6jE8kI6GxDiC9ugjEIjJz9DkhRtPn4cQdJZiElb1Bqv/b32Tj4F8YGpXZwl3q6Jw==",
-            // "X-Stamp:".$data['Stamp'],
-            "X-Stamp:1433510862",
-            // "X-Signature:".$data['hash']
-            "X-Signature:uSUzzbsG/QclgZUNBFxcuohad4aXDv/YN//aD6Ccmeo="
-        ),
+        CURLOPT_HTTPHEADER =>$header_data,
         CURLOPT_FOLLOWLOCATION => true
     );
     curl_setopt_array($ch, $curlOpts);
@@ -93,7 +101,7 @@ class Customer extends CI_Controller {
     // If there was an error, show it
     if (curl_error($ch)) die(curl_error($ch));
     curl_close($ch);
-    echo "something".$answer;
+    echo $answer;
 }
 }
 
--- application/libraries/customapi_lib.php
+++ application/libraries/customapi_lib.php
@@ -5,7 +5,8 @@ class Customapi_lib
 {
     protected $ci;
     protected $encrypted_key="54%&*$#$";
-    protected $bank_sec_key="FikC6CLB8nqG5A0B6ZGJPpGLCuK7Sa3M5vJkloKcnb66SwOGo1UKjnU6nQfC6eLc7O41AQZZOl29v4iklPjdVg==" ;
+    protected $bank_sec_key="162902e822c1f27a86e40d01e991893e918b0ae2bb49adcce6f26496829c9dbeba4b0386a3550a8e753a9d07c2e9e2dcecee350106593a5dbdbf88a494f8dd56" ;
+    // protected $bank_sec_key="FikC6CLB8nqG5A0B6ZGJPpGLCuK7Sa3M5vJkloKcnb66SwOGo1UKjnU6nQfC6eLc7O41AQZZOl29v4iklPjdVg==" ;
     protected $bank_app_key="eY/etUTuQV9SFC/nTXXT3S6jE8kI6GxDiC9ugjEIjJz9DkhRtPn4cQdJZiElb1Bqv/b32Tj4F8YGpXZwl3q6Jw==" ;
 	function __construct()
     {
@@ -32,15 +33,15 @@ class Customapi_lib
     }
     public function utcTimestamp()
     {
-        date_default_timezone_set("UTC");
+        // date_default_timezone_set("UTC");
         // return date("Y-m-d H:i:s", time()); 
         // $date = new DateTime();
         // return $date->getTimestamp();
         return time();
     }
-    public function createHmacsha($str='',$tec='sha256')
+    public function createHmacsha($str='',$tec='SHA256')
     {
-        // echo $enc_string = hash_hmac($tec, $str, $this->bank_app_key,true);
+        // echo $enc_string = hash_hmac($tec, $str, $this->bank_sec_key);
         $enc_string = hash_hmac($tec, $str, $this->bank_sec_key,true);
         return base64_encode($enc_string);
     }
